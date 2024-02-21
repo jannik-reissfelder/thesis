3e10db1c114a6bec0c3b7c7ee232ecba77c4ab46
jannik-reissfelder <jannik.reissfelder@gmail.com>
2024-02-21 11:57:31 +0100

    simplify evaluation script

--- evaluation.py
+++ evaluation.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import os
-from helper_functions.eval_functions import compute_distance_matrix, plot_heatmap
-from sklearn.decomposition import PCA
+from helper_functions.eval_functions import compute_distance_matrix, plot_heatmap, perform_pca_and_visualize, plot_sorted_df_line_distribution, perform_pcoa_and_visualize
 import numpy as np
-import plotly.express as px
-import matplotlib.pyplot as plt
+
 
 
 ## Load dataframe for evaltuation
@@ -18,9 +16,12 @@ files_predictions = os.listdir("./predictions/non-augmentation")
 
 # now load files and merge them into one dataframe
 predictions = pd.DataFrame()
-for file in files_predictions[:2]:
+for file in files_predictions:
     print("Loading file:", file)
     sub = pd.read_csv("./predictions/non-augmentation/" + file, index_col=0)
+    # rename columns to file name to keep track of the algorithms used
+    # replace 'predicted_' + col_name to 'file_name' + col_name
+    sub.columns = [col.replace('predicted_', file).replace('.csv', '_') for col in sub.columns]
     predictions = pd.concat([predictions, sub], axis=1)
 # transpose the dataframe
 predictions = predictions.T
@@ -30,75 +31,25 @@ class_to_evaluate = "Brassica napus"
 
 # get the predictions over the index that contains the class
 # Convert the index to string and filter rows where the index contains "Brassica napus"
-filtered_df = predictions[predictions.index.to_series().str.contains("Brassica napus")]
+predictions_eval_class = predictions[predictions.index.to_series().str.contains(class_to_evaluate)]
 
 # concatenate the filtered dataframe with the original dataframe
-df_with_prediction = pd.concat([df, filtered_df])
+df_with_prediction = pd.concat([df, predictions_eval_class])
 
 # Compute and plot  distance matrix based on metric
-# metric = 'wasserstein'
-# matrix = compute_distance_matrix(df_with_prediction, metric=metric)
-# plot_heatmap(matrix, title='BC - Matrix', annotations=True)
+metric = 'bray-curtis'
+matrix = compute_distance_matrix(df_with_prediction, metric=metric)
+plot_heatmap(matrix, title='BC - Matrix', annotations=True)
+
+# this plots the PCA of the original plant species vs 1 predicted plant species
+perform_pca_and_visualize(df, predictions_eval_class)
+# this plots the PCoA of the original plant species vs 1 predicted plant species
+perform_pcoa_and_visualize(matrix=matrix)
 
 
-### PCAAA #####
-# Perform PCA
-# num_components = 2
-#
-# pca = PCA(n_components=num_components)  # We'll look at the first two principal components
-# pca_result = pca.fit_transform(df)  # First project original points
-#
-# # transform predictions into same space as above
-# pca_result_pred = pca.transform(filtered_df)
-#
-# # concatenate two both pca results
-# pca_result_all = np.concatenate([pca_result, pca_result_pred])
-#
-#
-# # Create a DataFrame for the PCA results
-# pca_df = pd.DataFrame(pca_result_all, columns=['PC1', 'PC2'])
-#
-# # Add the sample names as a column
-# pca_df['sample'] = df_with_prediction.index # Adjust if necessary to match your data structure
-# # pca_df['sample_detail'] = df.index
-#
-# # Calculate the percentage of variance explained by each component
-# explained_var = pca.explained_variance_ratio_ * 100
-#
-# # Create an interactive scatter plot
-# fig = px.scatter(pca_df, x='PC1', y='PC2', text = None, color = "sample",
-#                  title=f'PCA of Microbiome Data (PC1: {explained_var[0]:.2f}%, PC2: {explained_var[1]:.2f}%)',
-#                  color_discrete_sequence=px.colors.qualitative.Plotly)
-#
-# # Improve layout
-# fig.update_traces(textposition='top center')
-# fig.update_layout(height=600, width=1200)
-#
-# # Show the figure
-# fig.show()
 
-
-def plot_sorted_df_line_distribution(df_plot):
-    plt.figure(figsize=(15, 10))  # Increased figure size for better clarity
-
-    # Sort the DataFrame based on the values of the first row
-    sorted_columns = df_plot.iloc[0].sort_values(ascending=False).index
-    sorted_df = df_plot[sorted_columns].iloc[:, :100] # TODO: this is just for the first 100 columns
-
-    for index, row in sorted_df.iterrows():
-        # Plot each row of the sorted DataFrame
-        plt.plot(row.values, label=f'Row {index}', linestyle='-', linewidth=1.5)
-
-    plt.title('Line Graph of Each Row After Sorting Based on First Row')
-    plt.xlabel('Column Index (Sorted)')
-    plt.ylabel('Value')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
-
-
-class_true_vs_predictions = df_with_prediction[df_with_prediction.index.to_series().str.contains("Brassica napus")]
-
+# this plots the abundance of the original plant species vs its predicted plant species
+class_true_vs_predictions = df_with_prediction[df_with_prediction.index.to_series().str.contains(class_to_evaluate)]
 plot_sorted_df_line_distribution(class_true_vs_predictions)
 
 
